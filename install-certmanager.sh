dd965f7c975d39b495bcda536a321df840dbc5fe
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-06-23 13:28:51 +0000

    wip

--- install-certmanager.sh
+++ install-certmanager.sh
@@ -1,3 +1,12 @@
-# operator-sdk olm install
-kubectl krew install operator
-kubectl operator install cert-manager -n operators --channel stable --approval Automatic
+# Install certmanager / openshift routes
+# 
+# 
+oc apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+oc apply -f https://github.com/cert-manager/openshift-routes/releases/latest/download/cert-manager-openshift-routes.yaml
+
+
+export NS="cert-manager"
+export EMAIL=$GITPOD_GIT_USER_EMAIL
+
+envsubst < "certmanager-issuer.env.k8s.yaml" > ".certmanager-issuer.k8s.yaml"
+oc apply -f ".certmanager-issuer.k8s.yaml"
