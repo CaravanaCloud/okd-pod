15ba59b4569ec0cec04cf262219429917f132ffa
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-01-02 17:45:21 +0000

    wip

--- create-cluster.aws-singlenode.sh
+++ create-cluster.aws-singlenode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -ex
+set -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 check_variables() {
   for var in "$@"; do
@@ -10,24 +11,26 @@ check_variables() {
   done
 }
 
-export AWS_REGION=${AWS_REGION:-"us-west-2"}
-export CLUSTER_NAME="okd-pod-$RANDOM"
+export AWS_REGION=${AWS_REGION:-"us-east-1"}
+export CLUSTER_NAME=${CLUSTER_NAME:-"okd$RANDOM"}
 export OCP_BASE_DOMAIN=${OCP_BASE_DOMAIN:-"devcluster.openshift.com"}
 export SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)
+export INSTANCE_TYPE=${INSTANCE_TYPE:-"m6i.2xlarge"}
 check_variables "AWS_REGION" "OCP_BASE_DOMAIN" "PULL_SECRET" "SSH_KEY"
 
 aws sts get-caller-identity
 envsubst < "install-config.aws-singlenode.env.yaml" > "install-config.yaml"
 cp "install-config.yaml" "install-config.bak.yaml"
 
-echo "WARNING: This will run 1 x m6i.2xlarge instances on your AWS account."
+echo "Creating cluster $CLUSTER_NAME"
+echo "WARNING: This will run 1 x $INSTANCE_TYPE instances on your AWS account."
 sleep 5
 
 openshift-install create cluster
 openshift-install wait-for install-complete
 
 mkdir -p "$HOME/.kube"
-ln -sf "/workspace/okd-pod/auth/kubeconfig" "$HOME/.kube/config"
+cp "$DIR/auth/kubeconfig" "$HOME/.kube/config"
 
 
 oc cluster-info
